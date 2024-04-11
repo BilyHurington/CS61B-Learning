a6d364b73f11534a00ced30c33675f95fae7c0c6
BilyHurington <bilyhurington@gmail.com>
2024-04-11 10:25:53 +0800

    HW1-Finish

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -1,19 +1,29 @@
 package synthesizer;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
-/** Tests the ArrayRingBuffer class.
- *  @author Josh Hug
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests the ArrayRingBuffer class.
+ *
+ * @author Josh Hug
  */
 
 public class TestArrayRingBuffer {
-    @Test
-    public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
-    }
+	/**
+	 * Calls tests for ArrayRingBuffer.
+	 */
+	public static void main(String[] args) {
+		jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
+	}
 
-    /** Calls tests for ArrayRingBuffer. */
-    public static void main(String[] args) {
-        jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
-    }
+	@Test
+	public void someTest() {
+		ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<Integer>(10);
+		arb.enqueue(10);
+		arb.enqueue(5);
+		assertEquals(arb.dequeue(), (Integer) 10);
+		assertEquals(arb.dequeue(), (Integer) 5);
+	}
 } 
--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -6,10 +6,16 @@ import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
 
+import javax.swing.text.Position;
 import java.util.Random;
 
 /**
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+	void addHexagonal(TETile[][] world, Position p, int size,TETile t){
+		for(int i=0;i<2*size;i++){
+			for(int j=0;j<2*size;j++){
+		}
+	}
 }
